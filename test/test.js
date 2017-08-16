ff2ba5ef578509158e600ce4b2c56bb70eb049db
Kristian Wahlroos <kristian.wahlroos@reaktor.com>
2017-08-16 11:03:13 +0300

    Add first unit test

--- test/test.js
+++ test/test.js
@@ -4,70 +4,49 @@ require([
   "./lib/annotations-rendering",
   "./lib/annotations-editing",
   "jquery",
-  "lodash"
+  "lodash",
 ], (mocha, chai, annotationRendering, annotationEditing, $, _) => {
+
   mocha.setup("bdd")
   const expect = chai.expect
+  const assert = chai.assert
+  const $answerContainer = $('.testAnswerContainer')
+  const richTextTemplate = (classes, content) => `<div class="${classes}">${content}</div>`
 
-  describe("Test selecting text", () => {
-          const annotation = {
-              message: 'great',
-              startIndex: 0,
-              length: 2
-          }
-          const annotations = [
-            annotation
-          ]
-          annotationRendering.renderGivenAnnotations($('.test'), annotations)
+  function setAnswer(content, cssClasses) {
+    const answerHtml = richTextTemplate(cssClasses, content)
+    $answerContainer.html(answerHtml)
+  }
 
-      it('should render annotation', () => {
-          expect($('.test').html()).to.equal('Tes')
-      })
+  function getAnnotationContent($answer) {
+    return $answer.find('.answerAnnotation')
+                    .toArray().map(e => e.textContent)
+  }
+
+  describe("When selecting richText", () => {
+    const annotations = [
+      {
+        message: 'great1',
+        startIndex: 0,
+        length: 5
+      },
+      {
+        message: 'great2',
+        startIndex: 1,
+        length: 5
+      }
+    ]
+    const answerContent = `answer rich <img alt="alt_image_text"> Text<br>Lorem ipsum<br><br><br>Vivamus venenatis<br><br><br>Phasellus tempus<br><br>Morbi<br><img alt="x">+<img alt="y"> = y + x`
+
+    before(() => setAnswer(answerContent, 'textAnswer richTextAnswer is_pregrading'))
+
+    it('first annotation should contain correct text', () => {
+        const firstAnnotation = annotations[0]
+        annotationRendering.renderGivenAnnotations($answerContainer.find('.textAnswer'), [firstAnnotation])
+        expect(getAnnotationContent($answerContainer)).to.include.members(['answe'])
+    })
   })
 
   mocha.run()
 
-  function addRangeAndTriggerPopup(answerTextSelector, start, end) {
-    const $answerText = $(answerTextSelector)
-    const nodes = $answerText.contents()
-    const selection = window.getSelection()
-
-    //TODO Once modularized use same logic as in production code by including also wrapped images
-    const nodeTextLengths = _.map(nodes, n => n.textContent.length)
-    const accumLengths = _.reduce(
-      nodeTextLengths,
-      (acc, n) => {
-        acc.push((acc.length > 0 ? acc[acc.length - 1] : 0) + n)
-        return acc
-      },
-      []
-    )
-    const accumulators = _.zipWith(nodes, accumLengths, (a, b) => ({
-      node: a,
-      length: b
-    }))
-
-    const offset = start
-    const startObject = findNodeObject(accumulators, offset)
-    const startNode = startObject.node
-    const startOffset =
-      startNode.textContent.length - (startObject.length - offset)
-
-    // TODO: node should never be an img node but the container
-    const endObject = findNodeObject(accumulators, offset + end)
-    const endNode = endObject.node
-    const endOffset =
-      endNode.textContent.length - (endObject.length - (end + offset))
-
-    const range = window.document.createRange()
-    range.setStart(startNode, startOffset)
-    range.setEnd(endNode, endOffset)
-    selection.removeAllRanges()
-    selection.addRange(range)
-    $answerText.mouseup()
-  }
-
-  function findNodeObject(nodes, length) {
-    return _.find(nodes, a => a.length >= length)
-  }
 })
